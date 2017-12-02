3a777a26316d37ecaef0b735c8daa74b04b1db04
Dmitriy <dima.komlyk@gmail.com>
2017-12-02 02:01:16 +0200

    Create Dmitriy_Komlyk.js

---
+++ Javascript Course/Homework/Lesson_1/Dmitriy_Komlyk.js
@@ -0,0 +1,13 @@
+function years(num) {
+  
+  if(num>=1&&num<=2017){
+   var result = Math.ceil(num/100);
+    return result; 
+  }
+  else{
+    alert('Год указан не правильно');
+  }
+  
+}
+
+years();
