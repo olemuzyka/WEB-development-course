4d9164f3ca8115523b486a223743c59ab628e8ec
Alaska90 <31796501+Alaska90@users.noreply.github.com>
2017-12-17 22:07:47 +0200

    Create Evgeniy_Kozlov.js

---
+++ Javascript Course/Homework/Lesson_4/Evgeniy_Kozlov.js
@@ -0,0 +1,21 @@
+function Array(newArray){
+  var sortArray = newArray.sort(function (a,b) {
+    return a-b;
+  });
+  var res = [];
+  var i = 0;
+  var j = i;
+  while(i<= sortArray.length-1){
+    if (sortArray[i]==sortArray[i+1]-1){
+      i++;
+    }  
+    
+      while(sortArray[j]<=sortArray[i+1]){
+      res.push(sortArray[j]);
+      sortArray[j] += 1; 
+     }
+      i++;
+  }
+  return res.length - sortArray.length;
+}
+Array([8, 2, 3, 6]);
