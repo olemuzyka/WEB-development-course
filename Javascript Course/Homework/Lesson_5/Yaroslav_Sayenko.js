16ca70ad483aeacc228a97f45ac4bee12dfbcaa5
Yaroslav Sayenko <sayenkoyaroslav@gmail.com>
2017-12-18 23:01:56 +0200

    fix bug, add arr. HW JS5

--- Javascript Course/Homework/Lesson_5/Yaroslav_Sayenko.js
+++ Javascript Course/Homework/Lesson_5/Yaroslav_Sayenko.js
@@ -2,7 +2,7 @@ function increase (arr) {
   var result = [];
   for (var i = 0; i < arr.length-1; i++) {
     if (arr[i] >= arr[i+1]) {
-      if (i !== 0 && arr[i-1] >= arr[i+1] && i+2 !== length-1 && arr[i] >= arr[i+2]) {
+      if (i !== 0 && arr[i-1] >= arr[i+1] && i+2 !== arr.length-1 && arr[i] >= arr[i+2]) {
         return false;
       }
       else {
