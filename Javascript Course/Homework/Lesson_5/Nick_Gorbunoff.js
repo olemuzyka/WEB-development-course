3f2e792ea057d3ac52ad44a3898eadecfb055056
NickGorbunoff <nick.gorbunoff2010@gmail.com>
2017-12-18 09:31:50 +0200

    Add new For with Splice

--- Javascript Course/Homework/Lesson_5/Nick_Gorbunoff.js
+++ Javascript Course/Homework/Lesson_5/Nick_Gorbunoff.js
@@ -1,16 +1,23 @@
 function checkArray(array){
   
+  for(var j=0; j<array.length; j++){
+    if(array[j] >= array[j+1]){
+      array.splice(j, 1);
+      break;
+    }
+  }
+
   var check=[];
   
   for(var i=0; i<array.length; i++){
-    if(array[i] > array[i+1]){
+    if(array[i] >= array[i+1]){
       check.push('false');
     } else {
       check.push('true');
     }
   }
   
-  if (check.filter(item => item === 'false').length > 1){
+  if (check.filter(item => item === 'false').length > 0){
     return false;
   } else {
     return true;
@@ -22,7 +29,7 @@ function checkArray(array){
   //   return item === 'false';
   // });
   
-  // if (a.length > 1){
+  // if (a.length > 0){
   //   return false;
   // } else {
   //   return true;
