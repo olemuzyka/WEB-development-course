0622fa56b273102c9dc8c439594fabb58122bfa3
NickGorbunoff <nick.gorbunoff2010@gmail.com>
2017-12-19 00:46:00 +0200

    Fix (remove splice and filter with arrow func), add new IF

--- Javascript Course/Homework/Lesson_5/Nick_Gorbunoff.js
+++ Javascript Course/Homework/Lesson_5/Nick_Gorbunoff.js
@@ -1,39 +1,22 @@
 function checkArray(array){
   
-  for(var j=0; j<array.length; j++){
-    if(array[j] >= array[j+1]){
-      array.splice(j+1, 1);
-      break;
-    }
-  }
-
-  var check=[];
+  var checkFirst=[];
+  var checkSecond=[];
   
-  for(var i=0; i<array.length; i++){
+  for(var i=0; i<array.length-1; i++){
     if(array[i] >= array[i+1]){
-      check.push('false');
-    } else {
-      check.push('true');
+      checkSecond.push('false');
+    } 
+    if(array[i-1] >= array[i+1]){
+      checkFirst.push('false');
     }
   }
   
-  if (check.filter(item => item === 'false').length > 0){
+  if (checkFirst.length <= 1 && checkSecond.length <= 1){
     return false;
   } else {
     return true;
   }
-
-  // Open Arrow functions
-
-  // var a = check.filter(function(item) {
-  //   return item === 'false';
-  // });
-  
-  // if (a.length > 0){
-  //   return false;
-  // } else {
-  //   return true;
-  // }
   
 }
 
