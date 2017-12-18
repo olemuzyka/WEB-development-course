01196bbf4b85fc74037a1d6dc65e1394c416a26e
Oleg Muzyka <olemuzyka@gmail.com>
2017-12-18 10:02:55 +0200

    Update Oleg_Muzyka_homework_4.js

--- Javascript Course/Homework/Lesson_4/Oleg_Muzyka_homework_4.js
+++ Javascript Course/Homework/Lesson_4/Oleg_Muzyka_homework_4.js
@@ -1,13 +1,15 @@
-var array = [6, 2, 3, 8];
-function fun(x){
-var sortArray = array.sort();
-var count = 0;
-    for (var i = 0; i < sortArray.length; i++){
-      if (sortArray[i+1] - sortArray[i] != 1){
-      count++;
-      } 
-    }
-    return count;    
-}
+var array = [5, 4, 26, 35];
+function compare(a,b) {return a - b};
+function fun(array){
+
+    var sortArray = array.sort(compare);
+    var count = [];
+    for (var i = sortArray[0]; i <= sortArray[sortArray.length-1]; i++){
+        count.push(i);
+       }
+   // console.log(count);
+    return count.length - sortArray.length;
+};
  
 fun(array);
+
