e8aae919fe98290dade1cf5722505a24f8081326
Dmitriy <dima.komlyk@gmail.com>
2017-12-19 12:29:55 +0200

    Update Dmitriy_Komlyk.js

--- Javascript Course/Homework/Lesson_5/Dmitriy_Komlyk.js
+++ Javascript Course/Homework/Lesson_5/Dmitriy_Komlyk.js
@@ -6,17 +6,20 @@ var removed;
  if(someArray.length<=1) return true;
     while(counter<=someArray.length-1){
        removed = someArray.splice(counter, 1);
-          for(var i = 0; i<=someArray.length-1; i++){
-             if(someArray[i]>=someArray[i+1]){ 
+         if(compare(someArray)) return true;
+       someArray.splice(counter, 0, removed[0]);
+    counter++;
+  }
+ function compare(arr){
+     for(var i = 0; i<=arr.length-1; i++){
+                 if(arr[i]>=arr[i+1]){ 
                    result = false;
                    break;
                  }
                  result = true;
           }
-         if(result) return true;
-       someArray.splice(counter, 0, removed[0]);
-    counter++;
-  }
+          return result;
+}
   return result;
 }
 filterArray([1, 3, 2, 1]);
