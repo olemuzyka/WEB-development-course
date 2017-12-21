76e93d19ecaeed99e17616bbd3103609af4bb235
Alexander Ponomariov <sanik.2008.2008@gmail.com>
2017-12-21 15:54:42 +0200

    Update Alexander_Ponomariov.js

--- Javascript Course/Homework/Lesson_5/Alexander_Ponomariov.js
+++ Javascript Course/Homework/Lesson_5/Alexander_Ponomariov.js
@@ -3,13 +3,16 @@ function isGrowAfterDel( numArr ) {
 	let quantity = 0;
 	
 	for (let i = 0; i < numArr.length; i++) {
-	  	let dif = numArr[i+1] - numArr[i];
-	  	dif <= 0 ? quantity++: '';
-	  	if ( quantity > 1 ) return false;
+	  	if (numArr[i] >= numArr[i+1]) {
+	  		quantity++
+	  	} 
+	  	if (numArr[i] >= numArr[i+2]) {
+	  		quantity++
+	  	} 
+	  	
+		if ( quantity>= 2 ) return false
 	}
 
 	return true;
 
 }
-
-isGrowAfterDel([1,3,2,5,10]);
