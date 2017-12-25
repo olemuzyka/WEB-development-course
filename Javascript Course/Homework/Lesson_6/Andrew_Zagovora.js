adf100b66e40ced0e3876432dc7e09d0227730d2
Andre Zagovora <andrezagovora@gmail.com>
2017-12-25 15:14:41 +0200

    Correct homework 5, 6 and 7

---
+++ Javascript Course/Homework/Lesson_6/Andrew_Zagovora.js
@@ -0,0 +1,23 @@
+function luckyTicket(n) {
+
+	if (n.length % 2 != 0 ) {
+		return false;
+	}
+
+	var sumLeft = 0;
+	for (i = 0; n.length/2 > i ; i++) {
+		var sumLeft = sumLeft + +n[i];
+	}
+
+	var sumRight = 0;
+	for (i ; n.length > i ; i++) {
+		var sumRight = sumRight + +n[i];
+	}
+
+	if (sumLeft == sumRight) {
+		return true;
+	} else {
+		return false;
+	}
+
+}
