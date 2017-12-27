02e9150b1087ee836f0c704b7e36e49ac63575f7
Dmitriy <dima.komlyk@gmail.com>
2017-12-27 11:20:55 +0200

    Create Dmitriy_Komlyk.js Lesson_8

---
+++ Javascript Course/Homework/Lesson_8/Dmitriy_Komlyk.js
@@ -0,0 +1,19 @@
+function filterString(someString){
+var result;
+var biggestArray = someString[0];
+
+function filterArray(Array, index){
+    return Array.length==biggestArray.length ;
+}
+ 
+for(var i=0; i< someString.length; i++){
+    if(someString[i].length > biggestArray.length){
+        biggestArray = someString[i];
+        result = someString.filter(filterArray);
+    }
+    if(someString[i].length<biggestArray.length) {result = someString.filter(filterArray)}
+}
+return result;
+}
+
+filterString(['aba', 'aa', 'ad', 'vcd', 'aba']);
