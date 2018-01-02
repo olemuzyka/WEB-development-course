13c88ee5749196d9949a57eee0358c3dd45e3f43
parshyn1988 <yuriyparshyn@gmail.com>
2018-01-02 16:54:11 +0200

    Update Yuriy_Parshyn.js
    Исправил

--- Javascript Course/Homework/Lesson_8/Yuriy_Parshyn.js
+++ Javascript Course/Homework/Lesson_8/Yuriy_Parshyn.js
@@ -1,15 +1,22 @@
-var arr = ['aba', 'aa', 'ad', 'vcd', 'aba'];
+var arr = ["abc", "eeee", "abcd", "dcd"];
+var arr1 = ["a", "abc", "cbd", "zzzzzz", "abcdef", "aaaaaa"];
+var arr2 = ["aba", "aa", "ad", "vcd", "aba"];
 function maxLengthWords (arr) {
-    var maxElement = 0;
-    var newArr = [];
-    for (var i = 0; i < arr.length; i++) {
-        if (maxElement < arr[i].length) {
-             maxElement = arr[i].length;
+            var maxElement = 0;
+            maxElement = arr.reduce(function(a, b) {
+                if (a.length > b.length) {
+                    return a.length;
+                }
+                else {
+                    return b.length;
+                }
+    });
+            var newArr = [];
+            for (var i = 0; i < arr.length; i++) {
+                    if (arr[i].length == maxElement){
+                            newArr.push(arr[i]);
+                        }
+                }
+            return newArr;
         }
-        if(arr[i].length == maxElement){
-            newArr.push(arr[i]);
-        }
-    }
-    return newArr;
-}
-console.log(maxLengthWords(arr));
+console.log(maxLengthWords(arr2));
