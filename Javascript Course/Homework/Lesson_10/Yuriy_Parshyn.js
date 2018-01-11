51bcbd4f500b61c739baf9bd77e3067c459eb68d
parshyn1988 <yuriyparshyn@gmail.com>
2018-01-11 16:42:06 +0200

    Update Yuriy_Parshyn.js

--- Javascript Course/Homework/Lesson_10/Yuriy_Parshyn.js
+++ Javascript Course/Homework/Lesson_10/Yuriy_Parshyn.js
@@ -1,23 +1,26 @@
-var a = [1,2,3];
-var b = [2,1,3];
+// var a = [832, 998, 148, 570, 533, 561, 894, 147, 455, 279];
+// var b = [832, 570, 148, 998, 533, 561, 455, 147, 894, 279];
+var a = [4,6,3];
+var b = [3,4,6];
 function compareArr(a,b) {
+  var flag = false;
   if (a.length != b.length) {
     return false;
   }
-  var count = 0;
   for (var i = 0; i < a.length; i++) {
-    for (var j = 0; j < b.length; j++) {
-      if (a[i] == b[j]) {
-        count++;
-        break;
+    if (a[i] != b[i]) {
+      if (!flag) {
+        for (var j = i + 1; j < b.length; j++) {
+          if (a[i] == b[j]) {
+            [b[i], b[j]] = [b[j], b[i]];
+          }
+        }
+      } else {
+        return false;
       }
+      flag = true;
     }
   }
-  if (count != a.length) {
-    return false;
-  }
-  else {
-    return true;
-  }
+  return true;
 }
 console.log(compareArr(a,b));
