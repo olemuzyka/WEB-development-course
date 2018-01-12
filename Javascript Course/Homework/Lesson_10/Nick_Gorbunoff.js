73a4f1b4eb2f8e9b49b2a4a9c47c8080448060b9
NickGorbunoff <nick.gorbunoff2010@gmail.com>
2018-01-12 11:49:42 +0200

    Rewrite

--- Javascript Course/Homework/Lesson_10/Nick_Gorbunoff.js
+++ Javascript Course/Homework/Lesson_10/Nick_Gorbunoff.js
@@ -1,33 +1,34 @@
 function sameArray(a, b){
 
+  var x = [];
+  var y = [];
+  var z = [];
+  
   if(a.length!==b.length){
     return false;
   }
-
-  var c = [];
-
+  
   for(var i=0; i<a.length; i++){
-    if(a[i]!==b[i]){
-      c.push('');
-    }
-    if(a[i]===b[i+1] || a[i+1]===b[i]){
-      c.push('');
-      i++;
-    }
-    if(a[i]===a[i+1] || b[i]===b[i+1]) {
-      c.push('');
-    }
-    if(b.indexOf(a[i]) == -1){
-      c.push('');
-    } 
-     
+    if(a[i]===b[i]) x.push(a[i]);
+    if(x.length===a.length) return true;
+  }
+
+  var c = a.slice(0);
+  var d = b.slice(0);
+
+  c.sort();
+  d.sort();
+  
+  for(var j=0; j<c.length; j++){
+    if(c[j]!==d[j]) return false;
   }
   
-  if(c.length<=2){
-    return true;
-  } else {
-    return false;
+  for(var k=0; k<a.length; k++){
+    if(a[k]!==b[k]) z.push(a[k]);
+    if(z.length>2) return false;
   }
+  
+  return true;
 
 }
 
