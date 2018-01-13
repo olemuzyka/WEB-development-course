9b63b50c62bf824797273b7b40233666f1d123bf
Dmitriy <dima.komlyk@gmail.com>
2018-01-13 20:46:54 +0200

    Update Dmitriy_Komlyk.js

--- Javascript Course/Homework/Lesson_10/Dmitriy_Komlyk.js
+++ Javascript Course/Homework/Lesson_10/Dmitriy_Komlyk.js
@@ -1,15 +1,12 @@
 function equalArrays(array1, array2){
-var counter = 0;
-while(counter < array1.length){
-  if(array1[counter]==array2[counter]){
-    counter++;
+  if(array1.length !== array2.length) return false;
+var differences = 0;
+for(var i = 0; i < array1.length; i++){
+  if(differences>2) return false;
+  if(array1[i]!==array2[i]){
+    differences++;
    }
-    else{
-      var a = array2.indexOf(array1[counter]);
-      array2[a] = array2.splice(counter,1, array2[a])[0];
-      break;
-    }
  }
-  return !(array1<array2 || array2<array1);
-  }
-equalArrays([1, 2, 3], [2, 1, 3]);
+  return true;
+ }
+equalArrays(a, b);
