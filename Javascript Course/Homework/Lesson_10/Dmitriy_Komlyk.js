2d6dbdc4e3e404c1764412fb42690ff31a33dcaa
Dmitriy <dima.komlyk@gmail.com>
2018-01-14 21:17:09 +0200

    Update Dmitriy_Komlyk.js

--- Javascript Course/Homework/Lesson_10/Dmitriy_Komlyk.js
+++ Javascript Course/Homework/Lesson_10/Dmitriy_Komlyk.js
@@ -2,7 +2,7 @@ function equalArrays(array1, array2){
   if(array1.length !== array2.length) return false;
 var differences = 0;
 for(var i = 0; i < array1.length; i++){
-  if(differences>2) return false;
+  if(differences>=2) return false;
   if(array1[i]!==array2[i]){
     differences++;
    }
