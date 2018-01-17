35c0cad980805fae37fbd0ad2aac05aa19a78447
Prochor Simon <prochorz@gmail.com>
2018-01-17 11:46:28 +0200

    HW10_Prochor_Simon_fix2

--- Javascript Course/Homework/Lesson_10/Prochor_Simon.js
+++ Javascript Course/Homework/Lesson_10/Prochor_Simon.js
@@ -1,42 +1,50 @@
 var arr1 = [1,2,3,4,5,6],
     arr2 = [1,4,3,2,5,6];
           
-function isSame(arr1, arr2) {
-  let odds = 0,
-      changedIndex = null,
-      passIndex = null;
-
-  if(arr1.length != arr2.length) { return false; }
-  
-  
-  for(let i = 0; i< arr1.length; i++) {
-    
-    if( arr1[i] !== arr2[i] ) {
-      if( odds===0 ) {
-        passIndex = arr2.indexOf(arr1[i], i + 1);
-        changedIndex = i;
-        
-        if( passIndex < 0 ) {
-          return false;
-        }
-      } else if(odds===1) {
-        if( i == passIndex ) {
-          if(arr1[i] != arr2[changedIndex]) {
-            return false;
+    function isSame(arr1, arr2) {
+      function findIndex(i) {
+          passIndex = arr2.indexOf(arr1[i], i + 1);
+          if( arr1[passIndex] === arr2[passIndex]) {
+              return findIndex(passIndex);
+          } else {
+              return passIndex;
           }
-        }else {
-          return false;
-        }
-      }else if( odds>1 ) {
-        return false;
       }
       
-      odds++;
-      
+    let odds = 0,
+        changedIndex = null,
+        passIndex = null;
+  
+    if(arr1.length != arr2.length) { return false; }
+    
+    
+    for(let i = 0; i< arr1.length; i++) {
+      if( arr1[i] !== arr2[i] ) {
+        if( odds===0 ) {
+          passIndex = findIndex(i);
+          changedIndex = i;
+          
+          if( passIndex < 0 ) {
+            return false;
+          }
+        } else if(odds===1) {
+          if( i == passIndex ) {
+            if(arr1[i] !== arr2[changedIndex]) {
+              return false;
+            }
+          }else {
+            return false;
+          }
+        }else if( odds>1 ) {
+          return false;
+        }
+        
+        odds++;
+        
+      }
     }
+        
+    return true;
   }
-      
-  return true;
-}
 
 isSame(arr1, arr2);
