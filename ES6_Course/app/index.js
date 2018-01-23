8488bc6d4f5db641bfc20ebb17c9c60a2b21fd64
Klim Trakht <klim@kultprosvet.net>
2018-01-23 23:58:19 +0200

    Add vue.js lesson 1

--- ES6_Course/app/index.js
+++ ES6_Course/app/index.js
@@ -802,15 +802,89 @@ fetch(root, {
     })
     .then(json => console.log(json));
 
+*/
 
+
+
+// let a = Math.pow(2, 5);
+// console.log(a);
+
+// let a = 2**5
+// console.log(a);
+
+
+// let b = "wonderful".includes("wonder");
+// console.log(b);
+
+// let b = [2, 3, 4, 5, 6].includes(4); // true
+// console.log(b);
+
+/*
+
+let obj = {
+    a: 'one',
+    b: 'two',
+    c: 'three'
+}
+
+let keys = Object.keys(obj);
+console.log(keys);
+
+
+let values = Object.values(obj);
+console.log(values);
+
+
+let entries = Object.entries(obj);
+console.log(entries);
 
 */
 
 
 
+// ES8 Proposal - Async
+/*
+
+async function async_one() {
+    return "one";
+}
 
 
+async_one().then(response => console.log(response));
 
 
+async function async_two() {
+    throw new Error('Issue with async!');
+}
 
 
+async_two().catch(error => console.log(error));
+
+
+*/
+
+
+function async_one() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('one');
+        }, 2000);
+    });
+
+}
+
+
+async function async_two() {
+    return "two"
+}
+
+
+async function async_three() {
+    const one = await async_one();
+    console.log(one);
+    const two = await async_two();
+    console.log(two);
+}
+
+
+async_three();
