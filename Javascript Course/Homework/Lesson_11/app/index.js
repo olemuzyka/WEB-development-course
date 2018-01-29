ead83b33f43c404d038fe6d34670a0227b490efd
AlexanderPonomariov <sanik.2008@gmail.com>
2018-01-29 18:37:01 +0200

    Adds reques and getting data from response

--- Javascript Course/Homework/Lesson_11/app/index.js
+++ Javascript Course/Homework/Lesson_11/app/index.js
@@ -5,14 +5,43 @@ document.addEventListener('DOMContentLoaded', () => {
 
         let searchString = document.getElementsByName('search-field');
 
-        console.log('Hello My friend');
-        console.log(e);
-        console.log(searchString);
-    })
+        // console.log('Hello My friend');
+        // console.log(e);
+        // console.log(searchString[0].value);
+
+        fetch(
+            'https://www.googleapis.com/books/v1/volumes?q=' + searchString[0].value,
+            {method:'get'},
+        ).then( response => {
+            // console.log(response);
+            return response.json();
+        }).then( books => {
+            // console.log(books.items);
+            let resultBlock = document.getElementById('result');
+            books.items.map(elem => {
+                console.log(elem);
+
+
+            // <ol id="list">
+            //         <li>0</li>
+            //         <li>1</li>
+            //         <li>2</li>
+            //         </ol>
+            //
+            //         <script>
+            //     var newLi = document.createElement('li');
+            //     newLi.innerHTML = 'Привет, мир!';
+            //
+            //     list.appendChild(newLi);
+            // </script>
+
+
+            });
+        });
 
 
 
+    });
 
 
-
-})
+});
