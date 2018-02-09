d9e9e44ec9ea968b0cec613b68d3bc0fd0f93623
Klim Trakht <klim@kultprosvet.net>
2018-02-09 10:41:50 +0200

    Add Vue.js Lesson 6 code

---
+++ Vue.js/turnout/src/components/Dashboard.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h3>Dashboard</h3>
+        <button class="btn btn-danger btn-sm signout-btn" @click="signOut">Sign Out</button>
+    </div>
+</template>
+
+<script>
+    import {firebaseApp} from "../firebaseApp";
+
+    export default {
+        name: 'Dashboard',
+        data () {
+            return {
+
+            }
+        },
+        methods: {
+            signOut(){
+                this.$store.dispatch('signOut')
+                firebaseApp.auth().signOut();
+            }
+        }
+    }
+</script>
---
+++ Vue.js/turnout/src/components/SignIn.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="form-inline">
+        <h3>SignIn</h3>
+
+        <div class="form-group">
+            <input
+                    type="text"
+                    placeholder="email"
+                    class="form-control"
+                    v-model="email"
+            >
+        </div>
+
+        <div class="form-group">
+            <input
+                    type="text"
+                    placeholder="password"
+                    class="form-control"
+                    v-model="password"
+            >
+        </div>
+
+        <br>
+
+        <button class="btn btn-primary" @click="signIn">Sign In</button>
+
+
+        <br>
+
+        <router-link to="/signup">Not a user ? Sign Up</router-link>
+
+        {{ error.message }}
+    </div>
+</template>
+
+<script>
+    import {firebaseApp} from "../firebaseApp";
+
+    export default {
+        name: 'SignIn',
+        data () {
+            return {
+                email: '',
+                password: '',
+                error: {
+                    message: ''
+                }
+            }
+        },
+        methods: {
+            signIn(){
+                firebaseApp.auth().signInWithEmailAndPassword(this.email, this.password)
+                    .catch(error => {
+                        this.error = error
+                    })
+            }
+        }
+    }
+</script>
